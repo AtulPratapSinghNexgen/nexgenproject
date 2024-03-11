09f53531807f54d65e1c839e20f7307302ba5267
AtulPratapSinghNexgen <atulp@nexgen.in>
2024-03-11 13:54:10 +0530

    Edit

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,7 +11,7 @@ Company Name : NexGen
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>NexGen | Welcome Atul</title> 
+     <title>NexGen | Welcome</title> 
     <link rel="stylesheet" href="{{url('asset/style.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
   </head>
